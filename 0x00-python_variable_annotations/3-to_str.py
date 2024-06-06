d9ffe28754eede165d3467e9fb8cc4c955b344e7
Sibongile <sibongile.nhlema@gmail.com>
2024-06-06 22:54:37 +0200

    created  function to_str that takes a float n as argument

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Type-annotated function to_str that takes a float n
+as argument and returns the string representation of the float
+'''
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (float): The input float.
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
