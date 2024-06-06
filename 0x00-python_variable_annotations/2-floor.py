2e21f1c2bc1f64edcdf6bea8ec9ef583f536e777
Sibongile <sibongile.nhlema@gmail.com>
2024-06-06 22:50:35 +0200

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' Module handles the implementation of math functions'''
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Args:
+        n (float): The input float
+    Returns:
+        int: The floor value of the input float
+    """
+    return math.floor(n)
