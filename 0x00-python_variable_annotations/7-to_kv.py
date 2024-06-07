da9a59392b6f53e84e6f19f1f02d0d2d484d4c4e
Sibongile <sibongile.nhlema@gmail.com>
2024-06-07 12:34:47 +0200

    fixed pycodestyle errors

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -9,7 +9,7 @@ should be annotated as a float.
 from typing import Union, Tuple
 
 
-def to_kv(k:str, v: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
     Args:
         k (str): string value given
