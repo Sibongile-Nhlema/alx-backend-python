2c587606ff72370889baa837d730fc6a8dc84c7d
Sibongile <sibongile.nhlema@gmail.com>
2024-06-07 22:21:59 +0200

    fixed pycodestyle errors

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ Augment the following code with the correct duck-typed annotations
 
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     '''
     Args:
