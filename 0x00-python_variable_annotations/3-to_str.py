e0f93ae00c9f79a4721a6b5e326c63064821174a
Sibongile <sibongile.nhlema@gmail.com>
2024-06-07 11:33:10 +0200

    completed task 5

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,10 +6,10 @@ as argument and returns the string representation of the float
 
 
 def to_str(n: float) -> str:
-    """
+    ''''
     Args:
         n (float): The input float.
     Returns:
         str: The string representation of the input float.
-    """
+    '''
     return str(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+Type-annotated function sum_list which takes a list
+(input_list) of floats as argument and returns their sum as a float.
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    Args:
+        input_list (List[float]): The list of floats to sum.
+    Returns:
+        float: The sum of the list of floats.
+    '''
+    return sum(input_list)
