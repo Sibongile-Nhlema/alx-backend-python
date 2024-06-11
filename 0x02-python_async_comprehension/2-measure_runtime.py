ffc845015be3e23d79a023c00dc3e55e7b0d8dee
Sibongile <sibongile.nhlema@gmail.com>
2024-06-11 20:40:06 +0200

    fixed pycodestyle errors

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,6 +11,7 @@ from typing import List
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     '''
     A coroutine that executes async_comprehension 4 times in parallel
