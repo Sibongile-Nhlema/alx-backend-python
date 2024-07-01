0e828d99dc31bcd108d2a7f065102201e369a97f
Sibongile <sibongile.nhlema@gmail.com>
2024-07-01 22:13:26 +0200

    resolved pycodetyle errors

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -9,6 +9,7 @@ from unittest.mock import patch
 from client import GithubOrgClient
 from parameterized import parameterized
 
+
 class TestGithubOrgClient(unittest.TestCase):
     '''
     Unit tests for GithubOrgClient class.
@@ -19,16 +20,17 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc",),
     ])
     @patch('client.get_json')
-    def test_org(self, org_name: str, mock_get_json: unittest.mock.Mock) -> None:
+    def test_org(self, org_name: str,
+                 mock_get_json: unittest.mock.Mock) -> None:
         '''
         Test that GithubOrgClient.org returns the correct value.
         '''
         client: GithubOrgClient = GithubOrgClient(org_name)
         mock_url: str = GithubOrgClient.ORG_URL.format(org=org_name)
-        
+
         mock_get_json.return_value = {"org_name": org_name}
         org_info: Dict[str, str] = client.org()
-        
+
         self.assertEqual(org_info["org_name"], org_name)
         mock_get_json.assert_called_once_with(mock_url)
 
