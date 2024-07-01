5c0f5f8d2c66d99fe48debae05372335dbe9bf7a
Sibongile <sibongile.nhlema@gmail.com>
2024-07-01 23:27:28 +0200

    wrote the method that tests if a repo has a licence

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -70,6 +70,17 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_repos_url.assert_called_once()
             mock_get_json.assert_called_once_with("https://api.github.com/orgs/test_org/repos")
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo: Dict, license_key: str, expected: bool) -> None:
+        '''
+        Tests the has_license method of GithubOrgClient
+        '''
+        result: bool = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
